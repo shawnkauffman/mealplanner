284f0a75b56497e693fa2b9bd309e955db990bdf
Shawn Kauffman <shawn.kauffman@gmail.com>
2019-02-03 13:48:10 -0500

    moved data up to main App component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,41 @@ import Footer from "./components/Footer";
 import MealList from "./components/MealList";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meals: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      meals: [
+        {
+          name: "Corned Beef & Cabbage",
+          url: "http://www.google.com",
+          ingredients: [
+            "corned beef",
+            "cabbage",
+            "vegetable broth",
+            "carrots",
+            "potatoes"
+          ]
+        },
+        {
+          name: "Pesto Pasta",
+          url: "http://www.apple.com",
+          ingredients: ["Pasta", "Pesto", "Mozzarella", "Sun dried tomatoes"]
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <main>
         <Header />
-        <MealList />
+        <MealList meals={this.state.meals} />
         <Footer />
       </main>
     );
