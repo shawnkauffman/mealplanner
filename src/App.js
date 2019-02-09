a23de28d2900c594a3c4a1b94126d9655e59d28e
Shawn Kauffman <shawn.kauffman@gmail.com>
2019-02-09 12:45:29 -0500

    added ability to shuffle/generate new meal plan

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     this.state = {
       meals: []
     };
+    this.generateNewMealPlan = this.generateNewMealPlan.bind(this);
   }
 
   componentDidMount() {
@@ -64,11 +65,39 @@ class App extends Component {
     });
   }
 
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  generateNewMealPlan() {
+    const newMeals = this.shuffle(this.state.meals);
+    this.setState({ meals: newMeals });
+  }
+
   render() {
     return (
       <main>
         <div id="modal" className="modal" />
-        <Header meals={this.state.meals} />
+        <Header
+          meals={this.state.meals}
+          generateNewMealPlan={this.generateNewMealPlan}
+        />
         <Router>
           <MealList path="/" meals={this.state.meals} />
           <DisplayAllMeals path="/meals/" meals={this.state.meals} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,9 @@ import { render } from "react-dom";
 import AddNewMealForm from "./AddNewMealForm";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.openAddNewMealForm = this.openAddNewMealForm.bind(this);
-    this.shuffle = this.shuffle.bind(this);
-  }
-
-  shuffle(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
-
   openAddNewMealForm() {
     document.getElementById("modal").classList.add("active");
     render(<AddNewMealForm />, document.getElementById("modal"));
-
-    const newMeals = this.shuffle(this.props.meals);
   }
 
   render() {
@@ -48,12 +20,14 @@ class Header extends Component {
               <a href="/meals/">Meals</a>
             </li>
             <li>
-              <a href="#">Create New Meal</a>
+              <button type="button" onClick={this.openAddNewMealForm}>
+                Create New Meal
+              </button>
             </li>
             <li>
               <button
                 type="button"
-                onClick={this.openAddNewMealForm}
+                onClick={this.props.generateNewMealPlan}
                 className="generate-btn"
               >
                 Generate Meal Plan
